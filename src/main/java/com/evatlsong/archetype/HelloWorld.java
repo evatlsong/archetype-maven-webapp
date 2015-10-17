0adf47caa0cd9f0d38e2cbb2ff5bf561feba2784
evatlsong <evatlsong@gmail.com>
2015-10-17 17:39:22 +0800

    basic test finish

--- src/main/java/com/evatlsong/archetype/HelloWorld.java
+++ src/main/java/com/evatlsong/archetype/HelloWorld.java
@@ -1,14 +1,17 @@
 package com.evatlsong.archetype;
 
-public class HelloWorld {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public String sayHello()
-	{
-		return "Hello World\n";
-	}
-	
-	public static void main(String[] args)
-	{
-		System.out.print( new HelloWorld().sayHello() );
-	}
+public class HelloWorld extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.write("hello world");
+        writer.close();
+    }
 }
--- src/test/java/com/evatlsong/archetype/HelloWorldTest.java
+++ src/test/java/com/evatlsong/archetype/HelloWorldTest.java
@@ -4,15 +4,16 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HelloWorldTest
-{
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class HelloWorldTest {
     @Test
-    public void testSayHello()
-    {
+    public void testSayHello() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
         HelloWorld helloWorld = new HelloWorld();
-
-        String result = helloWorld.sayHello();
-
-        assertEquals( "Hello World\n", result );
+        helloWorld.doGet(request, response);
+        assertEquals("hello world", response.getContentAsString());
     }
 }
