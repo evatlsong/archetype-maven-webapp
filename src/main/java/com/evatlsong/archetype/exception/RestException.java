0419970b7ae53cd55d6c2f9fa9b40c8b01c3b70a
evatlsong <evatlsong@gmail.com>
2015-12-23 17:43:27 +0800

    add exception handler with controlleradvice

---
+++ src/main/java/com/evatlsong/archetype/exception/RestException.java
@@ -0,0 +1,27 @@
+package com.evatlsong.archetype.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created by evatlsong on 15-12-23.
+ */
+public class RestException extends RuntimeException {
+
+    public HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+    public RestException() {
+    }
+
+    public RestException(HttpStatus status) {
+        this.status = status;
+    }
+
+    public RestException(String message) {
+        super(message);
+    }
+
+    public RestException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+}
--- src/main/java/com/evatlsong/archetype/web/HomeController.java
+++ src/main/java/com/evatlsong/archetype/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.evatlsong.archetype.web;
 
+import com.evatlsong.archetype.exception.RestException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,5 +14,15 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
         return "index";
+    }
+
+    @RequestMapping(value = "/rest-exception", method = RequestMethod.GET)
+    public String restException() {
+        throw new RestException(HttpStatus.METHOD_NOT_ALLOWED, "不同意");
+    }
+
+    @RequestMapping(value = "/null-pointer-exception", method = RequestMethod.GET)
+    public String nullPointException() {
+        throw new NullPointerException("null");
     }
 }
